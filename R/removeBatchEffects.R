2395de5d05d48785d3ae38651a1038eaf3235fe3
cfbeuchel <cfbeuchel@imise.uni-leipzig.de>
2019-05-06 15:13:22 +0200

    190506
    * fix check that fail checkBatchEffects.R when individuals get removed due
    to missingness in covariate data
    * fix use of wrong ID in removeBatchEffects.R

--- R/removeBatchEffects.R
+++ R/removeBatchEffects.R
@@ -458,9 +458,12 @@ removeBatchEffects = function(ht12object,
   ht12object$genesdetail = genesdetail
 
   # individuenattrib
+  sample_overview_l7[sample_overview_l7$in_study & !(sample_overview_l7$new_ID %in% good.ids), ]$reason4exclusion <- "tmp"
+  sample_overview_l7[!(sample_overview_l7$new_ID %in% good.ids),"in_study"] = F
 
-  sample_overview_l7[!(sample_overview_l7$new_ID %in% good.ids),"in_study"] = F #TAG#
-  sample_overview_l7[!(sample_overview_l7$new_ID %in% good.ids),"reason4exclusion"] <- "Missings in covariate data. This is not allowed with batch adjustment via ComBat()"
+  # don't overwrite the other reason4exclusions
+  sample_overview_l7[sample_overview_l7$reason4exclusion %in% "tmp" ,"reason4exclusion"] <- "Missings in covariate data. This is not allowed with batch adjustment via ComBat()"
+  # sample_overview_l7[!(sample_overview_l7$new_ID %in% good.ids),"reason4exclusion"] <- "Missings in covariate data. This is not allowed with batch adjustment via ComBat()"
   sample_overview_l7[sample_overview_l7$new_ID %in% singlbarcoders_ind,"in_study"] = F
   sample_overview_l7[sample_overview_l7$new_ID %in% singlbarcoders_ind,"reason4exclusion"] <- "Batch/effect correction via ComBat not possible - only 1 Ind. with this Sentrix ID"
 
